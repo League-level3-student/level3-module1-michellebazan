accdde61e77c075756faab9c31d262721097afcf
michellebazan <24michelle.b@gmail.com>
2021-06-22 16:55:48 -0700

    not doing the extra on memory, hashmaps!!

--- src/_05_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_05_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,7 +1,15 @@
 package _05_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_LogSearch implements ActionListener {
     /* 
      * Crate a HashMap of Integers for the keys and Strings for the values.
      * Create a GUI with three buttons. 
@@ -33,5 +41,79 @@ public class _02_LogSearch {
      *      If this ID exists in the HashMap, remove it. Otherwise, notify the
      *      user that the ID is not in the list. 
      */
+	JPanel panel;
+	JFrame frame;
+	JButton addEntry;
+	JButton searchID;
+	JButton viewList;
+	//JButton RemoveEntry; -- last
+	
+	HashMap <Integer, String> IDList = new HashMap <Integer, String>();
+	int[] keys;
+	static int q = 0;
+	int w = 0;
+	
+	public static void main(String[] args) {
+		 _02_LogSearch log = new  _02_LogSearch();
+		 log.start();
+	}
+	
+	public void start() {
+		panel = new JPanel();
+		frame = new JFrame("ID");
+		
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		addEntry = new JButton("Add Entry");
+		searchID = new JButton("Search By ID");
+		viewList = new JButton("View List");
+		
+		addEntry.addActionListener(this);
+		searchID.addActionListener(this);
+		viewList.addActionListener(this);
+		
+		panel.add(addEntry);
+		panel.add(searchID);
+		panel.add(viewList);
+		
+		frame.pack();
+	
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == addEntry) {
+			
+			String a = JOptionPane.showInputDialog("Enter ID Number");
+			int num = Integer.parseInt(a);
+			//note for next time - mess with static int here --- main error here
+			q = w;
+			keys[q] = num;
+			w++;
+			String nam = JOptionPane.showInputDialog("Enter a name");
+			IDList.put(num, nam);
+		
+		} else if (e.getSource() == searchID) {
+			
+			String b = JOptionPane.showInputDialog("Enter ID Number");
+			int numb = Integer.parseInt(b);
+			
+			if (IDList.containsKey(numb)) {
+				System.out.println(IDList.get(numb));
+			}
+			
+			
+		} else if (e.getSource() == viewList) {
+			//prints HashMap
+			for (int i = 0; i < q; i++) {
+				System.out.println("ID: " + keys[i] + "  Name: " + IDList.get(keys[i]));
+			}
+			
+			
+		}
+	}
 }
